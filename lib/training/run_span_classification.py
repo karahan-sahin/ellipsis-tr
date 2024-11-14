4c064625c4c69d8505928553ccf1f0dbc1a4957c
karahan-sahin <karahan.sahin@boun.edu.tr>
2024-11-14 18:54:51 +0300

    feat: enhance span classification by adding tokenization, NER tag creation, and model loading; update evaluation strategy

--- lib/training/run_span_classification.py
+++ lib/training/run_span_classification.py
@@ -1,4 +1,5 @@
 import os
+import torch
 import wandb
 import evaluate
 import pandas as pd
@@ -66,6 +67,18 @@ if __name__ == "__main__":
     val_df = pd.read_csv(args.dataset_file.replace('train', 'val'))
     test_df = pd.read_csv(args.dataset_file.replace('train', 'test'))
 
+    if args.extraction_type == 'discriminative':
+        label_list = [ 'O', 'B-ANTECEDENT', 'I-ANTECEDENT']
+
+    elif args.extraction_type == 'extractive':
+        label_list = [ "O" ] + [ f"B-{label}" for label in train_df.elliptical_type.unique() ] + [ f"I-{label}" for label in train_df.elliptical_type.unique() ]
+
+    label2id = {label: i for i, label in enumerate(label_list)}
+
+    # Load tokenizer and model
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+    model = AutoModelForTokenClassification.from_pretrained(args.model_name, num_labels=len(label_list))
+
     # Read tokenized_text and discriminative_span and extractive_span columns as lists,
     def read_literal(df):
         from ast import literal_eval
@@ -79,34 +92,78 @@ if __name__ == "__main__":
     val_df = read_literal(val_df)
     test_df = read_literal(test_df)
 
+    train_df['tokenized_text'] = train_df['candidate_text'].apply(tokenizer.tokenize)
+    val_df['tokenized_text'] = val_df['candidate_text'].apply(tokenizer.tokenize)
+    test_df['tokenized_text'] = test_df['candidate_text'].apply(tokenizer.tokenize)
+
+    train_df['tokenized_span'] = train_df['span'].apply(tokenizer.tokenize)
+    val_df['tokenized_span'] = val_df['span'].apply(tokenizer.tokenize)
+    test_df['tokenized_span'] = test_df['span'].apply(tokenizer.tokenize)
+
+    from string import punctuation
+    # Remove if punctuations are at the beginning or end of the span
+    def remove_punctuations(row):
+        if row['tokenized_span'][0] in punctuation:
+            row['tokenized_span'] = row['tokenized_span'][1:]
+        if row['tokenized_span'][-1] in punctuation:
+            row['tokenized_span'] = row['tokenized_span'][:-1]
+        return row
+    
+    train_df = train_df.apply(remove_punctuations, axis=1)
+    val_df = val_df.apply(remove_punctuations, axis=1)
+    test_df = test_df.apply(remove_punctuations, axis=1)
+
+    # Create ner_tags column
+    def create_ner_tags(row):
+        ner_tags = ['O'] * len(row['tokenized_text'])
+        for idx, token in enumerate(row['tokenized_text']):
+            if token == row['tokenized_span'][0]:
+                ner_tags[idx] = 'B'
+                for i in range(1, len(row['tokenized_span'])):
+                    if idx + i < len(row['tokenized_text']) and row['tokenized_text'][idx + i] == row['tokenized_span'][i]:
+                        ner_tags[idx + i] = 'I'
+                    else:
+                        break
+        return ner_tags
+    
+    train_df['ner_tags'] = train_df.apply(create_ner_tags, axis=1)
+    val_df['ner_tags'] = val_df.apply(create_ner_tags, axis=1)
+    test_df['ner_tags'] = test_df.apply(create_ner_tags, axis=1)
+
     # Reanme columns
     train_df = train_df.rename(columns={
         'tokenized_text': 'tokens',
-        'discriminative_span' if args.extraction_type == 'discriminative' else 'extractive_span': 'ner_tags'
     })
     val_df = val_df.rename(columns={
         'tokenized_text': 'tokens',
-        'discriminative_span' if args.extraction_type == 'discriminative' else 'extractive_span': 'ner_tags'
     })
     test_df = test_df.rename(columns={
         'tokenized_text': 'tokens',
-        'discriminative_span' if args.extraction_type == 'discriminative' else 'extractive_span': 'ner_tags'
     })
 
     if args.extraction_type == 'discriminative':
-        label_list = [ 'O', 'B-ANTECEDENT', 'I-ANTECEDENT']
         # Change the labels to B-ANTECEDENT and I-ANTECEDENT
         train_df['ner_tags'] = train_df['ner_tags'].apply(lambda x: [label_list[0] if label == 'O' else label_list[1] if label == 'B' else label_list[2] for label in x])
         val_df['ner_tags'] = val_df['ner_tags'].apply(lambda x: [label_list[0] if label == 'O' else label_list[1] if label == 'B' else label_list[2] for label in x])
         test_df['ner_tags'] = test_df['ner_tags'].apply(lambda x: [label_list[0] if label == 'O' else label_list[1] if label == 'B' else label_list[2] for label in x])
     
     elif args.extraction_type == 'extractive':
-        label_list = [ "O" ] + [ f"B-{label}" for label in train_df.elliptical_type.unique() ] + [ f"I-{label}" for label in train_df.elliptical_type.unique() ]
-
+        # Change the labels to B-{label} and I-{label}
+        train_df['ner_tags'] = train_df.apply(
+            lambda row: [label_list[0] if label == 'O' else f"B-{row['elliptical_type']}" if label == 'B' else f"I-{row['elliptical_type']}"  for label in row['ner_tags']],
+            axis=1
+        )
+        val_df['ner_tags'] = val_df.apply(
+            lambda row: [label_list[0] if label == 'O' else f"B-{row['elliptical_type']}" if label == 'B' else f"I-{row['elliptical_type']}"  for label in row['ner_tags']],
+            axis=1
+        )
+        test_df['ner_tags'] = test_df.apply(
+            lambda row: [label_list[0] if label == 'O' else f"B-{row['elliptical_type']}" if label == 'B' else f"I-{row['elliptical_type']}"  for label in row['ner_tags']],
+            axis=1
+        )
+    
     else:
         raise ValueError('Extraction type must be "discriminative" or "extractive"')
-
-    label2id = {label: i for i, label in enumerate(label_list)}
 
     # Print the first example
     def print_example(words, labels):
@@ -121,47 +178,36 @@ if __name__ == "__main__":
         print(line2)
         print('--'*20)
 
-    print_example(train_df['tokens'][0], train_df['ner_tags'][0])
-    print_example(val_df['tokens'][0], val_df['ner_tags'][0])
-    print_example(test_df['tokens'][0], test_df['ner_tags'][0])
+    print_example(train_df['tokens'].iloc[0], train_df['ner_tags'].iloc[0])
+    print_example(val_df['tokens'].iloc[0], val_df['ner_tags'].iloc[0])
+    print_example(test_df['tokens'].iloc[0], test_df['ner_tags'].iloc[0])
 
-    # Apply label2id to ner_tags
-    train_df['ner_tags'] = train_df['ner_tags'].apply(lambda x: [label2id[label] for label in x])
-    val_df['ner_tags'] = val_df['ner_tags'].apply(lambda x: [label2id[label] for label in x])
-    test_df['ner_tags'] = test_df['ner_tags'].apply(lambda x: [label2id[label] for label in x])
-    
     # Load dataset
     train_dataset = Dataset.from_pandas(train_df)
     val_dataset = Dataset.from_pandas(val_df)
     test_dataset = Dataset.from_pandas(test_df)
 
-    # Load tokenizer and model
-    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
-    model = AutoModelForTokenClassification.from_pretrained(args.model_name, num_labels=len(label_list))
-
     # # Tokenize dataset
     def tokenize_and_align_labels(examples):
         tokenized_inputs = tokenizer(
-            examples["tokens"],
-            padding=True, 
+            examples["candidate_text"],
+            padding='max_length', 
             truncation=True, 
-            is_split_into_words=True
+            max_length=256,
+            return_token_type_ids=True,
         )
         labels = []
         for i, label in enumerate(examples[f"ner_tags"]):
-            word_ids = tokenized_inputs.word_ids(batch_index=i)
-            previous_word_idx = None
             label_ids = []
-            for word_idx in word_ids:
-                if word_idx is None:
+            for token_idx, token_id in enumerate(tokenized_inputs['input_ids'][i]):
+                if token_id < 4: # CLS and SEP, PAD tokens
                     label_ids.append(-100)
-                elif word_idx != previous_word_idx:
-                    label_ids.append(label[word_idx])
                 else:
-                    label_ids.append(-100)
-                previous_word_idx = word_idx
+                    label_ids.append(label2id[label[token_idx-1]])
             labels.append(label_ids)
-        tokenized_inputs["labels"] = labels
+
+        tokenized_inputs["labels"] = torch.tensor(labels)
+
         return tokenized_inputs
 
     tokenized_datasets = {
@@ -183,11 +229,11 @@ if __name__ == "__main__":
             for prediction, label in zip(predictions, labels)
         ]
 
-        for i in range(len(true_labels)):
+        for i in range(min(len(true_labels), 20)):
             print('Model Predictions:')
-            print(print_example(tokenized_datasets['test']['tokens'][i], predictions[i]))
+            print(print_example(tokenized_datasets['validation']['tokens'][i], predictions[i]))
             print('True Labels:')
-            print(print_example(tokenized_datasets['test']['tokens'][i], true_labels[i]))
+            print(print_example(tokenized_datasets['validation']['tokens'][i], true_labels[i]))
             print('--'*20)
 
         results = metric.compute(predictions=predictions, references=true_labels)
@@ -206,7 +252,7 @@ if __name__ == "__main__":
         per_device_eval_batch_size=args.per_device_eval_batch_size,
         logging_steps=args.logging_steps,
         save_steps=args.save_steps,
-        evaluation_strategy="steps",
+        eval_strategy="steps",
         save_strategy='no',
         load_best_model_at_end=False,
         eval_steps=args.eval_steps,
